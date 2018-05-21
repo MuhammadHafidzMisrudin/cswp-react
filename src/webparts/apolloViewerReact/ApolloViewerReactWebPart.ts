86841d9470ff125a57c0455a4afe083d0609459b
Muhammad Hafidz <halfpheez92@gmail.com>
2018-05-21 10:32:14 +1000

    added a sharepoint project with react.

---
+++ src/webparts/apolloViewerReact/ApolloViewerReactWebPart.ts
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ApolloViewerReactWebPartStrings';
+import ApolloViewerReact from './components/ApolloViewerReact';
+import { IApolloViewerReactProps } from './components/IApolloViewerReactProps';
+
+export interface IApolloViewerReactWebPartProps {
+  description: string;
+}
+
+export default class ApolloViewerReactWebPart extends BaseClientSideWebPart<IApolloViewerReactWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IApolloViewerReactProps > = React.createElement(
+      ApolloViewerReact,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
